c3be2509102273a46fc908b680ff8280cf7c0e40
Guillermo Eduardo Vicente <gvicente@decahf.local>
2023-08-28 17:30:26 -0300

    Título : Fix bug / Refactor code / Style
    Tipo : fix 🐛 refactor ♻️ desing 🎨
    
    Descripción :
    - Se corrigen bugs en el archivos .js
    - Se refactoriza el código de update.js
    - Se refactoriza el código de contact.js
    - Se refactoriza los archivos main.scss header.scss y footer.scss

--- src/js/contact.js
+++ src/js/contact.js
@@ -27,47 +27,49 @@ form.addEventListener("submit", (event) => {
   let errorPhone = document.getElementById("errorPhone");
   let errorMessage = document.getElementById("errorMessage");
 
-  let nameRegex = /^[A-zA-Z ]{2,30}$/;
-  let lastNameRegex = /^[A-zA-Z ]{2,30}$/;
+  let nameRegex = /^[A-Z][ A-Za-z 0-9.']{0,30}$/;
+  let lastNameRegex = /^[A-Z][ A-Za-z 0-9.']{0,30}$/;
   let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  let messageRegex = /^[a-zA-Z0-9.,:;()¿?¡! ]{2,200}$/;
+  let phoneRegex = /^[0-9]{2,4}[-]{0,1}[0-9]{2,4}[-]{0,1}[0-9]{2,4}$/;
+  let messageRegex = /^[a-zA-Z0-9.,:;()¿?¡! ]{1,250}$/;
 
   let inputName = document.querySelector("#name");
   let inputLastName = document.querySelector("#lastName");
   let inputEmail = document.querySelector("#email");
+  let inputPhone = document.querySelector("#phone");
   let inputMessage = document.querySelector("#message");
 
-  if (name.length < 2 || name.length > 30) {
-    errorName.innerHTML = "El nombre debe tener entre 2 y 30 caracteres";
+  if (name.length < 0 || name.length > 30) {
+    errorName.innerHTML = "El nombre debe tener entre 1 y 30 caracteres";
     inputName.classList.add("is-invalid");
     validation = false;
   } else if (!nameRegex.test(name)) {
-    errorName.innerHTML = "El nombre debe tener caracteres válidos";
+    errorName.innerHTML = "El nombre debe comenzar en mayúsculas";
     inputName.classList.add("is-invalid");
     validation = false;
-    validationMessage.innerHTML += `<p>El nombre '${name}' no es válido</p>`;
+    validationMessage.innerHTML += `<p>El nombre ' ${name} ' no es válido</p>`;
   } else {
     errorName.innerHTML = "";
     inputName.classList.remove("is-invalid");
     inputName.classList.add("is-valid");
   }
 
-  if (lastName.length < 2 || lastName.length > 30) {
-    errorLastName.innerHTML = "El apellido debe tener entre 2 y 30 caracteres";
+  if (lastName.length < 0 || lastName.length > 30) {
+    errorLastName.innerHTML = "El apellido debe tener entre 1 y 30 caracteres";
     inputLastName.classList.add("is-invalid");
     validation = false;
   } else if (!lastNameRegex.test(lastName)) {
-    errorLastName.innerHTML = "El apellido debe tener caracteres válidos";
+    errorLastName.innerHTML = "El apellido debe comenzar en mayúsculas y tener tener caracteres válidos";
     inputLastName.classList.add("is-invalid");
     validation = false;
-    validationMessage.innerHTML += `<p>El apellido '${lastName}' no es válido</p>`;
+    validationMessage.innerHTML += `<p>El apellido ' ${lastName} ' no es válido</p>`;
   } else {
     errorLastName.innerHTML = "";
     inputLastName.classList.remove("is-invalid");
     inputLastName.classList.add("is-valid");
   }
 
-  if (email.length < 2 || email.length > 30) {
+  if (email.length < 0 || email.length > 30) {
     errorEmail.innerHTML = "El email debe tener entre 2 y 30 caracteres";
     inputEmail.classList.add("is-invalid");
     validation = false;
@@ -75,22 +77,37 @@ form.addEventListener("submit", (event) => {
     errorEmail.innerHTML = "El email debe tener caracteres válidos";
     inputEmail.classList.add("is-invalid");
     validation = false;
-    validationMessage.innerHTML += `<p>El email '${email}' no es válido</p>`;
+    validationMessage.innerHTML += `<p>El email ' ${email} ' no es válido</p>`;
   } else {
     errorEmail.innerHTML = "";
     inputEmail.classList.remove("is-invalid");
     inputEmail.classList.add("is-valid");
   }
 
-  if (message.length < 2 || message.length > 200) {
-    errorMessage.innerHTML = "El mensaje debe tener entre 2 y 200 caracteres";
+  if (phone.length < 0 || phone.length > 15) {
+    errorPhone.innerHTML = "El teléfono debe tener entre 8 y 15 caracteres";
+    inputPhone.classList.add("is-invalid");
+    validation = false;
+  } else if (!phoneRegex.test(phone)) {
+    errorPhone.innerHTML = "El teléfono debe tener caracteres válidos";
+    inputPhone.classList.add("is-invalid");
+    validation = false;
+    validationMessage.innerHTML += `<p>El teléfono ' ${phone} ' no es válido</p>`;
+  } else {
+    errorPhone.innerHTML = "";
+    inputPhone.classList.remove("is-invalid");
+    inputPhone.classList.add("is-valid");
+  }
+
+  if (message.length < 0 || message.length > 200) {
+    errorMessage.innerHTML = "El mensaje debe tener entre 1 y 200 caracteres";
     inputMessage.classList.add("is-invalid");
     validation = false;
   } else if (!messageRegex.test(message)) {
     errorMessage.innerHTML = "El mensaje debe tener caracteres válidos";
     inputMessage.classList.add("is-invalid");
     validation = false;
-    validationMessage.innerHTML += `<p>El mensaje '${message}' no es válido</p>`;
+    validationMessage.innerHTML += `<p>El mensaje ' ${message} ' no es válido</p>`;
   } else {
     errorMessage.innerHTML = "";
     inputMessage.classList.remove("is-invalid");
@@ -109,15 +126,15 @@ form.addEventListener("submit", (event) => {
         <p>Mensaje:'${message}'</p>
         <p>Te responderemos a la brevedad al siguiente correo: </p>
         <h3> ${email}</h3>
+        <p>Teléfono registrado: </p>
+        <h3> ${phone}</h3>
         <p>Saludos te desea Juguetería Cósmica,</p>
-        <span>Seguinos en las redes sociales!</span>`;
-
-  
+        <span>Seguinos en las redes sociales!</span>`
 
     setTimeout(() => {
       successMessage.innerHTML = "";
       form.style.display = "flex";
-    }, 3000);
+    }, 7000);
 
     form.reset();
   }
--- src/js/update.js
+++ src/js/update.js
@@ -1,146 +1,277 @@
-let form__update = document.querySelector("#form__update");
-let name_prod = document.querySelector("#name_prod");
-let price = document.querySelector("#price");
-let stock = document.querySelector("#stock");
-let brand = document.querySelector("#brand");
-let category = document.querySelector("#category");
-let short_description = document.querySelector("#short_description");
-let long_description = document.querySelector("#long_description");
-let free_shipping = document.querySelector("#free_shipping");
-let age_from = document.querySelector("#age_from");
-let age_to = document.querySelector("#age_to");
-let photo = document.querySelector("#photo");
-let btnSend = document.querySelector(".btnSend");
-let errors = document.querySelector("#errors");
-let successMessage = document.querySelector("#successMessage");
-let validationMessage = document.querySelector("#validationMessage");
+const form__update = document.querySelector("#form__update");
 
-form__update.addEventListener("submit", function (e) {
-    e.preventDefault();
-    let errors = [];
-    // let successMessage = [];
-    // let validationMessage = [];
+let validationMessage = document.getElementById("validationMessage");
+let successMessage = document.getElementById("successMessage");
 
-    if (name_prod.value.length < 5) {
-        errors.push("El nombre debe tener al menos 5 caracteres");
-        name_prod.classList.add("is-invalid");
-    } else {
-        name_prod.classList.add("is-valid");
-    }
+form__update.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    if (price.value.length < 1) {
-        errors.push("El precio debe tener al menos 1 caracter");
-        price.classList.add("is-invalid");
-    } else {
-        price.classList.add("is-valid");
-    }
+  let validation = true;
+  validationMessage.innerHTML = "";
 
-    if (stock.value.length < 1) {
-        errors.push("El stock debe tener al menos 1 caracter");
-        stock.classList.add("is-invalid");
-    } else {
-        stock.classList.add("is-valid");
-    }
+  let name_prod = e.target.name_prod.value;
+  let price = e.target.price.value;
+  let stock = e.target.stock.value;
+  let brand = e.target.brand.value;
+  let category = e.target.category.value;
+  let short_description = e.target.short_description.value;
+  let long_description = e.target.long_description.value;
+  let free_shipping = e.target.free_shipping.value;
+  let age_from = e.target.age_from.value;
+  let age_to = e.target.age_to.value;
+  let photo = e.target.photo.value;
 
-    if (brand.value.length < 1) {
-        errors.push("La marca debe tener al menos 1 caracter");
-        brand.classList.add("is-invalid");
-    } else {
-        brand.classList.add("is-valid");
-    }
+  let errorName_prod = document.getElementById("errorName_prod");
+  let errorPrice = document.getElementById("errorPrice");
+  let errorStock = document.getElementById("errorStock");
+  let errorBrand = document.getElementById("errorBrand");
+  let errorCategory = document.getElementById("errorCategory");
+  let errorShort_description = document.getElementById(
+    "errorShort_description"
+  );
+  let errorLong_description = document.getElementById("errorLong_description");
+  let errorFree_shipping = document.getElementById("errorFree_shipping");
+  let errorAge_from = document.getElementById("errorAge_from");
+  let errorAge_to = document.getElementById("errorAge_to");
+  let errorPhoto = document.getElementById("errorPhoto");
+  let errorMessage = document.getElementById("errorMessage");
 
-    if (category.value.length < 1) {
-        errors.push("La categoria debe tener al menos 1 caracter");
-        category.classList.add("is-invalid");
-    } else {
-        category.classList.add("is-valid");
-    }
+  let name_prodRegex = /^[A-Z 0-9][ A-Za-z 0-9.']{0,30}$/;
+  let priceRegex = /^[0-9]+([.][0-9]+)?$/;
+  let stockRegex = /^[0-9]+$/;
+  let brandRegex = /^[A-Z 0-9][ A-Za-z 0-9.']{0,30}$/;
+  let categoryRegex = /^[A-Z][ A-Za-z 0-9.']{0,30}$/;
+  let short_descriptionRegex = /^[A-Za-z0-9\s]{1,30}$/;
+  let long_descriptionRegex = /^[A-Za-z0-9\s]{1,30}$/;
+  let free_shippingRegex = /^[A-Za-z0-9\s]{1,30}$/;
+  let age_fromRegex = /^[0-9]{1,2}$/;
+  let age_toRegex = /^[0-9]{1,2}$/;
+  let photoRegex = /^[A-Za-z0-9\s]{1,30}$/;
 
-    if (short_description.value.length < 1) {
-        errors.push("La descripcion corta debe tener al menos 1 caracter");
-        short_description.classList.add("is-invalid");
-    } else {
-        short_description.classList.add("is-valid");
-    }
+  let inputName_prod = document.querySelector("#name_prod");
+  let inputPrice = document.querySelector("#price");
+  let inputStock = document.querySelector("#stock");
+  let inputBrand = document.querySelector("#brand");
+  let inputCategory = document.querySelector("#category");
+  let inputShort_description = document.querySelector("#short_description");
+  let inputLong_description = document.querySelector("#long_description");
+  let inputFree_shipping = document.querySelector("#free_shipping");
+  let inputAge_from = document.querySelector("#age_from");
+  let inputAge_to = document.querySelector("#age_to");
+  let inputPhoto = document.querySelector("#photo");
 
-    if (long_description.value.length < 1) {
-        errors.push("La descripcion larga debe tener al menos 1 caracter");
-        long_description.classList.add("is-invalid");
-    } else {
-        long_description.classList.add("is-valid");
-    }
+  if (name_prod.length < 0 || name_prod.length > 30) {
+    errorName_prod.innerHTML = "El nombre debe tener entre 1 y 30 caracteres";
+    inputName_prod.classList.add("is-invalid");
+    validation = false;
+  } else if (!name_prodRegex.test(name_prod)) {
+    errorName_prod.innerHTML = "El nombre debe comenzar en mayúsculas";
+    inputName_prod.classList.add("is-invalid");
+    validation = false;
+    validationMessage.innerHTML += `<p>El nombre ' ${name_prod} ' no es válido</p>`;
+  } else {
+    errorName_prod.innerHTML = "";
+    inputName_prod.classList.remove("is-invalid");
+    inputName_prod.classList.add("is-valid");
+  }
 
-    if (free_shipping.value.length < 1) {
-        errors.push("El envio sin cargo debe tener al menos 1 caracter");
-        free_shipping.classList.add("is-invalid");
-    } else {
-        free_shipping.classList.add("is-valid");
-    }
+  if (price.length < 0 || price.length > 30) {
+    errorPrice.innerHTML = "El precio debe tener entre 1 y 30 caracteres";
+    inputPrice.classList.add("is-invalid");
+    validation = false;
+  } else if (!priceRegex.test(price)) {
+    errorPrice.innerHTML = "El precio debe tener caracteres válidos";
+    inputPrice.classList.add("is-invalid");
+    validation = false;
+    validationMessage.innerHTML += `<p>El precio ' ${price} ' no es válido</p>`;
+  } else {
+    errorPrice.innerHTML = "";
+    inputPrice.classList.remove("is-invalid");
+    inputPrice.classList.add("is-valid");
+  }
 
-    if (age_from.value.length < 1) {
-        errors.push("La edad desde debe tener al menos 1 caracter");
-        age_from.classList.add("is-invalid");
-    } else {
-        age_from.classList.add("is-valid");
-    }
+  if (stock.length < 0 || stock.length > 30) {
+    errorStock.innerHTML = "El stock debe tener entre 1 y 30 caracteres";
+    inputStock.classList.add("is-invalid");
+    validation = false;
+  } else if (!stockRegex.test(stock)) {
+    errorStock.innerHTML = "El stock debe tener caracteres válidos";
+    inputStock.classList.add("is-invalid");
+    validation = false;
+    validationMessage.innerHTML += `<p>El stock ' ${stock} ' no es válido</p>`;
+  } else {
+    errorStock.innerHTML = "";
+    inputStock.classList.remove("is-invalid");
+    inputStock.classList.add("is-valid");
+  }
 
-    if (age_to.value.length < 1) {
-        errors.push("La edad hasta debe tener al menos 1 caracter");
-        age_to.classList.add("is-invalid");
-    } else {
-        age_to.classList.add("is-valid");
-    }
+  if (brand.length < 0 || brand.length > 30) {
+    errorBrand.innerHTML = "La marca debe tener entre 1 y 30 caracteres";
+    inputBrand.classList.add("is-invalid");
+    validation = false;
+  } else if (!brandRegex.test(brand)) {
+    errorBrand.innerHTML = "La marca debe tener caracteres válidos";
+    inputBrand.classList.add("is-invalid");
+    validation = false;
+    validationMessage.innerHTML += `<p>La marca ' ${brand} ' no es válido</p>`;
+  } else {
+    errorBrand.innerHTML = "";
+    inputBrand.classList.remove("is-invalid");
+    inputBrand.classList.add("is-valid");
+  }
 
-    if (photo.value.length < 1) {
-        errors.push("La foto debe tener al menos 1 caracter");
-        photo.classList.add("is-invalid");
-    } else {
-        photo.classList.add("is-valid");
-    }
+  if (category.length < 1 || category.length > 30) {
+    errorCategory.innerHTML = "La categoría debe tener entre 1 y 30 caracteres";
+    inputCategory.classList.add("is-invalid");
+    validation = false;
+  } else if (!categoryRegex.test(category)) {
+    errorCategory.innerHTML = "La categoría debe tener caracteres válidos";
+    inputCategory.classList.add("is-invalid");
+    validation = false;
+    validationMessage.innerHTML += `<p>La categoría ' ${category} ' no es válido</p>`;
+  } else {
+    errorCategory.innerHTML = "";
+    inputCategory.classList.remove("is-invalid");
+    inputCategory.classList.add("is-valid");
+  }
 
-    if (errors.length > 0) {
-        e.preventDefault();
-        let ulErrors = document.querySelector("#errors");
-        ulErrors.innerHTML = "";
-        for (let i = 0; i < errors.length; i++) {
-            ulErrors.innerHTML += `<li>${errors[i]}</li>`;
-        }
-    } else {
-        let successMessage = document.querySelector("#successMessage");
-        successMessage.innerHTML = "El producto se ha agregado correctamente";
-        successMessage.classList.add("is-valid");
-        form__update.style.display = "none";
-        let validationMessage = document.querySelector("#validationMessage");
+  if (short_description.length < 2 || short_description.length > 30) {
+    errorShort_description.innerHTML =
+      "La descripción corta debe tener entre 2 y 30 caracteres";
+    inputShort_description.classList.add("is-invalid");
+    validation = false;
+  } else if (!short_descriptionRegex.test(short_description)) {
+    errorShort_description.innerHTML =
+      "La descripción corta debe tener caracteres válidos";
+    inputShort_description.classList.add("is-invalid");
+    validation = false;
+    validationMessage.innerHTML += `<p>La descripción corta ' ${short_description} ' no es válido</p>`;
+  } else {
+    errorShort_description.innerHTML = "";
+    inputShort_description.classList.remove("is-invalid");
+    inputShort_description.classList.add("is-valid");
+  }
 
-        let product = {
-            name_prod: name_prod.value,
-            price: price.value,
-            stock: stock.value,
-            brand: brand.value,
-            category: category.value,
-            short_description: short_description.value,
-            long_description: long_description.value,
-            free_shipping: free_shipping.value,
-            age_from: age_from.value,
-            age_to: age_to.value,
-            photo: photo.value
-        }
+  if (long_description.length < 2 || long_description.length > 30) {
+    errorLong_description.innerHTML =
+      "La descripción larga debe tener entre 2 y 30 caracteres";
+    inputLong_description.classList.add("is-invalid");
+    validation = false;
+  } else if (!long_descriptionRegex.test(long_description)) {
+    errorLong_description.innerHTML =
+      "La descripción larga debe tener caracteres válidos";
+    inputLong_description.classList.add("is-invalid");
+    validation = false;
+    validationMessage.innerHTML += `<p>La descripción larga ' ${long_description} ' no es válido</p>`;
+  } else {
+    errorLong_description.innerHTML = "";
+    inputLong_description.classList.remove("is-invalid");
+    inputLong_description.classList.add("is-valid");
+  }
 
-        validationMessage.innerHTML = `<p>Producto agregado</p>
-        <p>Nombre: ${product.name_prod} </p>
-        <p>Precio: ${product.price} </p>>
-        <p>Stock: ${product.stock}</p>
-        <p>Marca: ${product.brand} </p>
-        <p>Categoria: ${product.category} </p>
-        <p>Descripcion Corta: ${product.short_description} </p>
-        <p>Descripcion Larga: ${product.long_description} </p>
-        <p>Envio sin Cargo: ${product.free_shipping} </p>
-        <p>Edad Desde: ${product.age_from} </p>
-        <p>Edad Hasta: ${product.age_to} </p>
-        <p>Foto: ${product.photo} </p>`;
-        validationMessage.classList.add("is-valid");
+  // if (free_shipping.length < 2 || free_shipping.length > 30) {
+  //   errorFree_shipping.innerHTML =
+  //     "El envío gratis debe tener entre 2 y 30 caracteres";
+  //   inputFree_shipping.classList.add("is-invalid");
+  //   validation = false;
+  // } else if (!free_shippingRegex.test(free_shipping)) {
+  //   errorFree_shipping.innerHTML =
+  //     "El envío gratis debe tener caracteres válidos";
+  //   inputFree_shipping.classList.add("is-invalid");
+  //   validation = false;
+  //   validationMessage.innerHTML += `<p>El envío gratis '${free_shipping}' no es válido</p>`;
+  // } else {
+  //   errorFree_shipping.innerHTML = "";
+  //   inputFree_shipping.classList.remove("is-invalid");
+  //   inputFree_shipping.classList.add("is-valid");
+  // }
 
-        form__update.reset();
-    }
+  if (age_from.length < 0 || age_from.length > 3) {
+    errorAge_from.innerHTML = "La edad desde debe tener entre 1 y 3 caracteres";
+    inputAge_from.classList.add("is-invalid");
+    validation = false;
+  } else if (!age_fromRegex.test(age_from)) {
+    errorAge_from.innerHTML = "La edad desde debe tener caracteres válidos";
+    inputAge_from.classList.add("is-invalid");
+    validation = false;
+    validationMessage.innerHTML += `<p>La edad desde ' ${age_from} ' no es válido</p>`;
+  } else {
+    errorAge_from.innerHTML = "";
+    inputAge_from.classList.remove("is-invalid");
+    inputAge_from.classList.add("is-valid");
+  }
 
+  if (age_to.length < 0 || age_to.length > 3) {
+    errorAge_to.innerHTML = "La edad hasta debe tener entre 1 y 3 caracteres";
+    inputAge_to.classList.add("is-invalid");
+    validation = false;
+  } else if (!age_toRegex.test(age_to)) {
+    errorAge_to.innerHTML = "La edad hasta debe tener caracteres válidos";
+    inputAge_to.classList.add("is-invalid");
+    validation = false;
+    validationMessage.innerHTML += `<p>La edad hasta ' ${age_to} ' no es válido</p>`;
+  } else {
+    errorAge_to.innerHTML = "";
+    inputAge_to.classList.remove("is-invalid");
+    inputAge_to.classList.add("is-valid");
+  }
+
+  // if (photo.length < 2 || photo.length > 30) {
+  //   errorPhoto.innerHTML =
+  //     "La foto debe tener entre 2 y 30 caracteres";
+  //   inputPhoto.classList.add("is-invalid");
+  //   validation = false;
+  // } else if (!photoRegex.test(photo)) {
+  //   errorPhoto.innerHTML =
+  //     "La foto debe tener caracteres válidos";
+  //   inputPhoto.classList.add("is-invalid");
+  //   validation = false;
+  //   validationMessage.innerHTML += `<p>La foto '${photo}' no es válido</p>`;
+  // } else {
+  //   errorPhoto.innerHTML = "";
+  //   inputPhoto.classList.remove("is-invalid");
+  //   inputPhoto.classList.add("is-valid");
+  // }
+
+  if (validation) {
+    successMessage.innerHTML = "El producto se ha actualizado correctamente";
+    // successMessage.classList.remove("d-none");
+    // successMessage.classList.add("d-block");
+    form__update.style.display = "none";
+
+    let product = {
+      name_prod: name_prod.value,
+      price: price.value,
+      stock: stock.value,
+      brand: brand.value,
+      category: category.value,
+      short_description: short_description.value,
+      long_description: long_description.value,
+      free_shipping: free_shipping.value,
+      age_from: age_from.value,
+      age_to: age_to.value,
+      photo: photo.value,
+    };
+
+    validationMessage.innerHTML = `<p>Detalle de Producto agregado</p>
+        <p>Nombre: ${name_prod} </p>
+        <p>Precio: ${price} </p>
+        <p>Stock: ${stock}</p>
+        <p>Marca: ${brand} </p>
+        <p>Categoria: ${category} </p>
+        <p>Descripcion Corta: ${short_description} </p>
+        <p>Descripcion Larga: ${long_description} </p>
+        <p>Envio sin Cargo: ${free_shipping} </p>
+        <p>Edad Desde: ${age_from} </p>
+        <p>Edad Hasta: ${age_to} </p>
+        <p>Foto: ${photo} </p>`;
+    validationMessage.classList.add("is-valid");
+
+    setTimeout(() => {
+      successMessage.innerHTML = "";
+      form__update.submit();
+    }, 7000);
+
+    form__update.reset();
+  }
 });
